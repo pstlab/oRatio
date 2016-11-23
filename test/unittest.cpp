530384a42dfa65f495e8b3e90369eb0452089950
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2016-11-23 16:16:11 +0100

    minor test improvements..

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -13,14 +13,21 @@ public:
 
 		ac::arith_var* x0 = n.new_real();
 		Assert::IsNotNull(x0);
+		Assert::AreEqual(-std::numeric_limits<double>::infinity(), x0->to_interval().lb);
+		Assert::AreEqual(std::numeric_limits<double>::infinity(), x0->to_interval().ub);
+
 		ac::arith_var* x1 = n.new_real();
 		Assert::IsNotNull(x1);
+		Assert::AreEqual(-std::numeric_limits<double>::infinity(), x1->to_interval().lb);
+		Assert::AreEqual(std::numeric_limits<double>::infinity(), x1->to_interval().ub);
 
 		bool added = n.add({ n.geq(x1, x0), n.geq(x0, n.new_real(0)) });
 		Assert::IsTrue(added);
 
-		Assert::AreEqual(0, x0->to_interval().lb, 0);
-		Assert::AreEqual(std::numeric_limits<double>::infinity(), x0->to_interval().lb, 0);
+		Assert::AreEqual(0.0, x0->to_interval().lb);
+		Assert::AreEqual(std::numeric_limits<double>::infinity(), x0->to_interval().ub);
+		Assert::AreEqual(0.0, x1->to_interval().lb);
+		Assert::AreEqual(std::numeric_limits<double>::infinity(), x1->to_interval().ub);
 	}
 
 	TEST_METHOD(TestSolver) {
