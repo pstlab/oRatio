79b683c18b497f3ed75a1e0cbec0e6c5a1bbfc03
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2016-11-23 12:34:33 +0100

    minor cleanings..

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -1,11 +1,16 @@
-#include "stdafx.h"
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#include "../solver/solver.h"
+
 namespace test {
 	TEST_CLASS(UnitTest1) {
 public:
+
+	TEST_METHOD(TestAC) {
+		ac::network n;
+	}
 
 	TEST_METHOD(TestMethod1) {
 		oratio::solver s;
