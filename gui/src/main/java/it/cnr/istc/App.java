6925a02e0da0287f41ae719b909fe5c03b51fc3e
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2019-01-29 11:37:31 +0100

    minor cleanings..

--- gui/src/main/java/it/cnr/istc/App.java
+++ gui/src/main/java/it/cnr/istc/App.java
@@ -15,6 +15,10 @@ import javafx.stage.WindowEvent;
  */
 public class App extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
         Context.getContext().getService().getCore()
--- gui/src/main/java/it/cnr/istc/Launcher.java
+++ gui/src/main/java/it/cnr/istc/Launcher.java
@@ -6,6 +6,6 @@ package it.cnr.istc;
 public class Launcher {
 
     public static void main(String[] args) {
-        App.launch(args);
+        App.main(args);
     }
 }
