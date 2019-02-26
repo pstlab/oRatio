7f8fe7780f729b23eacdbbdf71aad15c280dfd61
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2019-02-26 16:00:04 +0100

    added log list to gui..

--- gui/src/main/java/it/cnr/istc/MainScene.java
+++ gui/src/main/java/it/cnr/istc/MainScene.java
@@ -2,7 +2,11 @@ package it.cnr.istc;
 
 import it.cnr.istc.oratio.StateListener;
 import it.cnr.istc.oratio.riddle.Core;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 /**
@@ -10,12 +14,18 @@ import javafx.scene.layout.VBox;
  */
 public class MainScene extends Scene implements StateListener {
 
+    private final ObservableList<String> logs = FXCollections.observableArrayList();
+    private final ListView<String> list = new ListView<>(logs);
+
     public MainScene() {
-        super(new VBox(), 400, 300);
+        super(new VBox(), 600, 400);
+        ((VBox) getRoot()).getChildren().add(list);
+        VBox.setVgrow(list, Priority.ALWAYS);
     }
 
     @Override
     public void log(String log) {
+        logs.add(log);
     }
 
     @Override
