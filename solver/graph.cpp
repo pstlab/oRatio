0b16fc5b6906dd91732e4ceece7f2cb6366d75dd
Riccardo De Benedictis <riccardodebenedictis@Plannings-MacBook-Pro.local>
2019-03-27 12:17:05 +0100

    minor causal graph improvements..

--- solver/graph.cpp
+++ solver/graph.cpp
@@ -6,9 +6,10 @@ namespace ratio
 graph::graph(solver &slv) : slv(slv), gamma(slv.get_sat_core().new_var()) {}
 graph::~graph() {}
 
-flaw::flaw(graph &gr) : gr(gr) {}
+flaw::flaw(graph &gr, const std::vector<resolver *> &causes, const bool &exclusive) : gr(gr), causes(causes), supports(causes), exclusive(exclusive) {}
 flaw::~flaw() {}
 
-resolver::resolver(graph &gr) : gr(gr) {}
+resolver::resolver(graph &gr, const smt::rational &cost, flaw &eff) : resolver(gr, gr.slv.get_sat_core().new_var(), cost, eff) {}
+resolver::resolver(graph &gr, const smt::var &r, const smt::rational &cost, flaw &eff) : gr(gr), rho(r), intrinsic_cost(cost), effect(eff) {}
 resolver::~resolver() {}
 } // namespace ratio
--- solver/graph.h
+++ solver/graph.h
@@ -18,10 +18,15 @@ class resolver;
 
 class graph
 {
+  friend class flaw;
+  friend class resolver;
+
 public:
   graph(solver &slv);
   graph(const graph &that) = delete;
   ~graph();
+
+  solver &get_solver() { return slv; }
 
 private:
   solver &slv;
@@ -32,22 +37,39 @@ private:
 class flaw
 {
 public:
-  flaw(graph &gr);
+  flaw(graph &gr, const std::vector<resolver *> &causes, const bool &exclusive = false);
   flaw(const flaw &that) = delete;
   ~flaw();
 
+  graph &get_graph() { return gr; }
+
 private:
   graph &gr;
+  smt::var phi;                                              // the propositional variable indicating whether the flaw is active or not..
+  smt::rational est_cost = smt::rational::POSITIVE_INFINITY; // the estimated cost of the flaw..
+  std::vector<resolver *> resolvers;                         // the resolvers for this flaw..
+  std::vector<resolver *> causes;                            // the causes for having this flaw..
+  std::vector<resolver *> supports;                          // the resolvers supported by this flaw..
+  const bool exclusive;                                      // a boolean indicating whether the flaw is exclusive (i.e. exactly one of its resolver can be applied)..
+  bool expanded = false;                                     // a boolean indicating whether the flaw has been expanded..
 };
 
 class resolver
 {
 public:
-  resolver(graph &gr);
+  resolver(graph &gr, const smt::rational &cost, flaw &eff);
+  resolver(graph &gr, const smt::var &r, const smt::rational &cost, flaw &eff);
   resolver(const resolver &that) = delete;
   ~resolver();
 
+  graph &get_graph() { return gr; }
+
 private:
   graph &gr;
+  const smt::var rho;                                        // the propositional variable indicating whether the resolver is active or not..
+  const smt::rational intrinsic_cost;                        // the intrinsic cost of the resolver..
+  smt::rational est_cost = smt::rational::POSITIVE_INFINITY; // the estimated cost of the resolver, computed by the heuristic, except for the intrinsic cost..
+  flaw &effect;                                              // the flaw solved by this resolver..
+  std::vector<flaw *> preconditions;                         // the preconditions of this resolver..
 };
 } // namespace ratio
