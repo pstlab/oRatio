c326f826007377ab65612efd243075850b42f10d
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2019-07-19 11:02:12 +0200

    minor cleanings..

--- solver/flaw.h
+++ solver/flaw.h
@@ -17,62 +17,65 @@ class resolver;
 
 class flaw
 {
-    friend class solver;
-    friend class graph;
-    friend class resolver;
+  friend class solver;
+  friend class graph;
+  friend class resolver;
 
 public:
-    flaw(graph &gr, const std::vector<resolver *> &causes, const bool &exclusive = false);
-    flaw(const flaw &that) = delete;
-    ~flaw();
+  flaw(graph &gr, const std::vector<resolver *> &causes, const bool &exclusive = false);
+  flaw(const flaw &that) = delete;
+  ~flaw();
 
-    graph &get_graph() const { return gr; }
-    smt::var get_phi() const { return phi; }
-    const std::vector<resolver *> &get_resolvers() const { return resolvers; }
-    const std::vector<resolver *> &get_causes() const { return causes; }
-    const std::vector<resolver *> &get_supports() const { return supports; }
-    bool is_expanded() const { return expanded; }
+  graph &get_graph() const { return gr; }
+  smt::var get_phi() const { return phi; }
+  const std::vector<resolver *> &get_resolvers() const { return resolvers; }
+  const std::vector<resolver *> &get_causes() const { return causes; }
+  const std::vector<resolver *> &get_supports() const { return supports; }
 
-    smt::rational get_estimated_cost() const { return est_cost; }
-    resolver *get_best_resolver() const;
+  smt::rational get_estimated_cost() const { return est_cost; }
+  bool is_deferrable() const { return deferrable; }
+  bool is_expanded() const { return expanded; }
+
+  resolver *get_best_resolver() const;
 
 #ifdef BUILD_GUI
-    virtual std::string get_label() const = 0;
+  virtual std::string get_label() const = 0;
 #endif
 
 private:
-    /**
+  /**
    * Initializes this flaw.
    * 
    * @pre the solver must be at root-level.
    */
-    void init();
-    /**
+  void init();
+  /**
    * Expands this flaw, invoking the compute_resolvers procedure.
    * 
    * @pre the solver must be at root-level.
    */
-    void expand();
-    virtual void compute_resolvers() = 0;
+  void expand();
+  virtual void compute_resolvers() = 0;
 
 protected:
-    /**
+  /**
    * Adds the resolver 'r' to this flaw.
    */
-    void add_resolver(resolver &r);
-    /**
+  void add_resolver(resolver &r);
+  /**
    * Adds this flaw to the preconditions of the resolver 'r'.
    */
-    void make_precondition_of(resolver &r);
+  void make_precondition_of(resolver &r);
 
 private:
-    graph &gr;                                                 // the graph this flaw belongs to..
-    smt::var phi;                                              // the propositional variable indicating whether the flaw is active or not..
-    smt::rational est_cost = smt::rational::POSITIVE_INFINITY; // the estimated cost of the flaw..
-    std::vector<resolver *> resolvers;                         // the resolvers for this flaw..
-    const std::vector<resolver *> causes;                      // the causes for having this flaw (used for activating the flaw through causal propagation)..
-    std::vector<resolver *> supports;                          // the resolvers supported by this flaw (used for propagating cost estimates)..
-    const bool exclusive;                                      // a boolean indicating whether the flaw is exclusive (i.e. exactly one of its resolver can be applied)..
-    bool expanded = false;                                     // a boolean indicating whether the flaw has been expanded..
+  graph &gr;                                                 // the graph this flaw belongs to..
+  smt::var phi;                                              // the propositional variable indicating whether the flaw is active or not..
+  smt::rational est_cost = smt::rational::POSITIVE_INFINITY; // the current estimated cost of the flaw..
+  bool deferrable = false;                                   // a boolean indicating whether the flaw is deferrable..
+  bool expanded = false;                                     // a boolean indicating whether the flaw has been expanded..
+  std::vector<resolver *> resolvers;                         // the resolvers for this flaw..
+  const std::vector<resolver *> causes;                      // the causes for having this flaw (used for activating the flaw through causal propagation)..
+  std::vector<resolver *> supports;                          // the resolvers supported by this flaw (used for propagating cost estimates)..
+  const bool exclusive;                                      // a boolean indicating whether the flaw is exclusive (i.e. exactly one of its resolver can be applied)..
 };
 } // namespace ratio
--- solver/resolver.h
+++ solver/resolver.h
@@ -17,36 +17,41 @@ class flaw;
 
 class resolver
 {
-    friend class solver;
-    friend class graph;
-    friend class flaw;
+  friend class solver;
+  friend class graph;
+  friend class flaw;
 
 public:
-    resolver(graph &gr, const smt::rational &cost, flaw &eff);
-    resolver(graph &gr, const smt::var &r, const smt::rational &cost, flaw &eff);
-    resolver(const resolver &that) = delete;
-    ~resolver();
+  resolver(graph &gr, const smt::rational &cost, flaw &eff);
+  resolver(graph &gr, const smt::var &r, const smt::rational &cost, flaw &eff);
+  resolver(const resolver &that) = delete;
+  ~resolver();
 
-    graph &get_graph() const { return gr; }
-    smt::var get_rho() const { return rho; }
-    smt::rational get_intrinsic_cost() const { return intrinsic_cost; }
-    smt::rational get_estimated_cost() const { return est_cost + intrinsic_cost; }
-    flaw &get_effect() const { return effect; }
-    const std::vector<flaw *> &get_preconditions() const { return preconditions; }
+  graph &get_graph() const { return gr; }
+  smt::var get_rho() const { return rho; }
+  smt::rational get_intrinsic_cost() const { return intrinsic_cost; }
+  smt::rational get_estimated_cost() const { return est_cost + intrinsic_cost; }
+  flaw &get_effect() const { return effect; }
+  const std::vector<flaw *> &get_preconditions() const { return preconditions; }
 
 #ifdef BUILD_GUI
-    virtual std::string get_label() const = 0;
+  virtual std::string get_label() const = 0;
 #endif
 
 private:
-    virtual void apply() = 0;
+  /**
+   * Applies this resolver, introducing subgoals and/or constraints.
+   * 
+   * @pre the solver must be at root-level.
+   */
+  virtual void apply() = 0;
 
 private:
-    graph &gr;                                                 // the graph this resolver belongs to..
-    const smt::var rho;                                        // the propositional variable indicating whether the resolver is active or not..
-    const smt::rational intrinsic_cost;                        // the intrinsic cost of the resolver..
-    smt::rational est_cost = smt::rational::POSITIVE_INFINITY; // the estimated cost of the resolver, computed by the heuristic, except for the intrinsic cost..
-    flaw &effect;                                              // the flaw solved by this resolver..
-    std::vector<flaw *> preconditions;                         // the preconditions of this resolver..
+  graph &gr;                                                 // the graph this resolver belongs to..
+  const smt::var rho;                                        // the propositional variable indicating whether the resolver is active or not..
+  const smt::rational intrinsic_cost;                        // the intrinsic cost of the resolver..
+  smt::rational est_cost = smt::rational::POSITIVE_INFINITY; // the estimated cost of the resolver, computed by the heuristic, except for the intrinsic cost..
+  flaw &effect;                                              // the flaw solved by this resolver..
+  std::vector<flaw *> preconditions;                         // the preconditions of this resolver..
 };
 } // namespace ratio
