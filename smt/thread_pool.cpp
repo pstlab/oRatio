8d46c1de922f1daeec73b7d2d525255d1de3d64f
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2020-01-31 18:22:52 +0100

    trying to solve compatibilityi issues..

--- smt/thread_pool.cpp
+++ smt/thread_pool.cpp
@@ -9,7 +9,6 @@ thread_pool::thread_pool()
     unsigned c_size = std::thread::hardware_concurrency();
     workers.reserve(c_size);
     for (unsigned i = 0; i < c_size; i++)
-    {
         workers.emplace_back([this, i] {
             while (true)
             {
@@ -25,20 +24,19 @@ thread_pool::thread_pool()
                 }
                 task();
                 {
-                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+                    std::lock_guard<std::mutex> lock(this->queue_mutex);
                     active--;
                     if (active == 0)
                         condition.notify_all();
                 }
             }
         });
-    }
 }
 
 thread_pool::~thread_pool()
 {
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+        std::lock_guard<std::mutex> lock(queue_mutex);
         stop = true;
     }
     condition.notify_all();
@@ -49,7 +47,7 @@ thread_pool::~thread_pool()
 void thread_pool::enqueue(std::function<void()> f)
 {
     {
-        std::unique_lock<std::mutex> lock(this->queue_mutex);
+        std::lock_guard<std::mutex> lock(this->queue_mutex);
         this->tasks.push(f);
     }
     this->condition.notify_one();
