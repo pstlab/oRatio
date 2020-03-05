f510bbd92d6956611e0f3cafce2769fb7c8b892d
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2020-03-05 15:53:23 +0100

    added simple test for integer difference logic theory..

--- smt/tests/test_time.cpp
+++ smt/tests/test_time.cpp
@@ -1,4 +1,5 @@
 #include "temporal_network.h"
+#include "idl_theory.h"
 #include "rdl_theory.h"
 #include <cassert>
 
@@ -41,6 +42,69 @@ void test_temporal_network_1()
     size_t tp0 = tn.new_tp();
     tn.add_constraint(temporal_network<rational>::origin(), tp0, 0, 10);
     auto bnd_0 = tn.bound(tp0);
+}
+
+void test_integer_distance_logic()
+{
+    sat_core core;
+    idl_theory idl(core, 5);
+    var origin = idl.new_var();
+    var horizon = idl.new_var();
+    bool nd = idl.distance(horizon, origin, 0);
+    assert(nd);
+
+    var tp0 = idl.new_var();
+    nd = idl.distance(tp0, origin, 0);
+    assert(nd);
+    nd = idl.distance(horizon, tp0, 0);
+    assert(nd);
+
+    var tp1 = idl.new_var();
+    nd = idl.distance(tp1, origin, 0);
+    assert(nd);
+    nd = idl.distance(horizon, tp1, 0);
+    assert(nd);
+
+    var tp2 = idl.new_var();
+    nd = idl.distance(tp2, origin, 0);
+    assert(nd);
+    nd = idl.distance(horizon, tp2, 0);
+    assert(nd);
+
+    nd = idl.distance(tp0, tp1, 10);
+    assert(nd);
+    nd = idl.distance(tp1, tp0, 0);
+    assert(nd);
+
+    nd = idl.distance(tp1, tp2, 10);
+    assert(nd);
+    nd = idl.distance(tp2, tp1, 0);
+    assert(nd);
+
+    nd = idl.distance(origin, tp0, 10);
+    assert(nd);
+    nd = idl.distance(tp0, origin, 0);
+    assert(nd);
+
+    var tp2_after_40 = idl.new_distance(tp2, origin, -40);
+    assert(tp2_after_40 == FALSE_var);
+
+    var tp2_after_20 = idl.new_distance(tp2, origin, -20);
+    var tp2_before_20 = idl.new_distance(origin, tp2, 20);
+
+    var tp2_after_30 = idl.new_distance(tp2, origin, -30);
+    var tp2_before_30 = idl.new_distance(origin, tp2, 30);
+
+    nd = core.disj({core.new_conj({tp2_after_20, tp2_before_20}), core.new_conj({tp2_after_30, tp2_before_30})});
+    assert(nd);
+
+    nd = idl.distance(origin, tp2, 30);
+    assert(nd);
+    nd = idl.distance(tp2, origin, -25);
+    assert(nd);
+
+    bool ch = core.check();
+    assert(ch);
 }
 
 void test_real_distance_logic()
@@ -111,5 +175,6 @@ int main(int, char **)
     test_temporal_network_0();
     test_temporal_network_1();
 
+    test_integer_distance_logic();
     test_real_distance_logic();
 }
