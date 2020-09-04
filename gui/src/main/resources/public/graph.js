95dbaeea7abd24967884e510229f93cad8feb35f
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2020-09-04 16:51:56 +0200

    solved some initial issues..

--- gui/src/main/resources/public/graph.js
+++ gui/src/main/resources/public/graph.js
@@ -1,15 +1,15 @@
 const nodes = [{ id: 0, label: 'a' }, { id: 1, label: 'b' }, { id: 2, label: 'c' }];
 const links = [{ source: 0, target: 1, type: 'a' }, { source: 1, target: 2, type: 'b' }];
 
-const vis = d3.select('#vis');
-const width = vis.attr('width'), height = vis.attr('height');
+const svg = d3.select('#graph').append('svg');
+const height = document.body.clientHeight
+const width = document.body.clientWidth
 
-let zoom = d3.zoom().on("zoom", (event) => {
-    vis.attr("transform",
-        `translate(${event.transform.x}, ${event.transform.y})` + " " +
-        `scale(${event.transform.k})`);
+let zoom = d3.zoom().on("zoom", event => {
+    svg.attr("transform", event.transform);
+    //svg.attr("transform",        `translate(${event.transform.x}, ${event.transform.y})` + " " +        `scale(${event.transform.k})`);
 });
-vis.call(zoom);
+svg.call(zoom);
 
 const simulation = d3.forceSimulation(nodes)
     .force('link', d3.forceLink().id(d => d.id))
@@ -19,17 +19,20 @@ const simulation = d3.forceSimulation(nodes)
 var rainbow = d3.scaleSequential(d3.interpolateRainbow);
 
 function update() {
-    const n_group = vis.selectAll('g').data(nodes, d => d.id);
-    const entering_nodes = n_group.enter().append('g').call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended));
-    entering_nodes.append('rect').attr('width', 30).attr('x', -15).attr('height', 10).attr('y', -5).attr('rx', 5).attr('ry', 5).style('fill', 'pink');
-    entering_nodes.append('text').text(d => d.label);
-
-    const l_group = vis.selectAll('line').data(links);
-    const entering_links = l_group.enter().append('line').attr('stroke', 'black');
+    const n_group = svg.selectAll('g').data(nodes, d => d.id).join(
+        enter => {
+            const g = enter.append('g').attr("cursor", "grab");
+            g.append('rect').attr('width', 30).attr('x', -15).attr('height', 10).attr('y', -5).attr('rx', 5).attr('ry', 5).style('fill', 'pink');
+            g.append('text').text(d => d.label);
+            g.call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended));
+            return g;
+        }
+    );
+    const l_group = svg.selectAll('line').data(links).join(enter => enter.append('line').attr('stroke', 'black'));
 
     simulation.nodes(nodes).on('tick', () => {
-        l_group.attr('x1', d => d.source.x).attr('y1', d => d.source.y).attr('x2', d => d.target.x).attr('y2', d => d.target.y);
         n_group.attr('transform', d => `translate(${d.x}, ${d.y})`);
+        l_group.attr('x1', d => d.source.x).attr('y1', d => d.source.y).attr('x2', d => d.target.x).attr('y2', d => d.target.y);
     });
     simulation.force("link").links(links);
 }
@@ -42,20 +45,21 @@ function addNode(n) {
     nodes.push(n);
     update();
     simulation.restart();
-    simulation.alpha(1);
+    simulation.alpha(0.3);
 }
 
 function addLink(n0, n1) {
     links.push({ source: n0.id, target: n1.id, type: 'c' });
     update();
     simulation.restart();
-    simulation.alpha(1);
+    simulation.alpha(0.3);
 }
 
 function dragstarted(event, d) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
+    d3.select(this).attr("cursor", "grabbing");
 }
 
 function dragged(event, d) {
@@ -67,4 +71,5 @@ function dragended(event, d) {
     if (!event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
+    d3.select(this).attr("cursor", "grab");
 }
