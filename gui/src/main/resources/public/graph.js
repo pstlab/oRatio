08fbddfb4b2fcd45e954bd799cc240364fe22d11
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2020-09-06 12:41:34 +0200

    solved issue with zoom..

--- gui/src/main/resources/public/graph.js
+++ gui/src/main/resources/public/graph.js
@@ -1,40 +1,37 @@
 const nodes = [{ id: 0, label: 'a' }, { id: 1, label: 'b' }, { id: 2, label: 'c' }];
 const links = [{ source: 0, target: 1, type: 'a' }, { source: 1, target: 2, type: 'b' }];
 
-const svg = d3.select('#graph').append('svg');
-const height = document.body.clientHeight
-const width = document.body.clientWidth
+const svg = d3.select("svg");
+const g = svg.append("g");
 
-let zoom = d3.zoom().on("zoom", event => {
-    svg.attr("transform", event.transform);
-    //svg.attr("transform",        `translate(${event.transform.x}, ${event.transform.y})` + " " +        `scale(${event.transform.k})`);
-});
-svg.call(zoom);
+const b_box = svg.node().getBoundingClientRect();
+const width = b_box.width, height = b_box.height;
+
+let c_zoom = d3.zoom().on('zoom', event => g.attr('transform', event.transform));
+svg.call(c_zoom);
 
 const simulation = d3.forceSimulation(nodes)
     .force('link', d3.forceLink().id(d => d.id))
     .force('charge', d3.forceManyBody())
     .force('center', d3.forceCenter(width / 2, height / 2));
 
-var rainbow = d3.scaleSequential(d3.interpolateRainbow);
-
 function update() {
-    const n_group = svg.selectAll('g').data(nodes, d => d.id).join(
+    const n_group = g.selectAll('g').data(nodes, d => d.id).join(
         enter => {
-            const g = enter.append('g').attr("cursor", "grab");
+            const g = enter.append('g').attr('cursor', 'grab');
             g.append('rect').attr('width', 30).attr('x', -15).attr('height', 10).attr('y', -5).attr('rx', 5).attr('ry', 5).style('fill', 'pink');
             g.append('text').text(d => d.label);
             g.call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended));
             return g;
         }
     );
-    const l_group = svg.selectAll('line').data(links).join(enter => enter.append('line').attr('stroke', 'black'));
+    const l_group = g.selectAll('line').data(links).join(enter => enter.append('line').attr('stroke', 'black'));
 
     simulation.nodes(nodes).on('tick', () => {
         n_group.attr('transform', d => `translate(${d.x}, ${d.y})`);
         l_group.attr('x1', d => d.source.x).attr('y1', d => d.source.y).attr('x2', d => d.target.x).attr('y2', d => d.target.y);
     });
-    simulation.force("link").links(links);
+    simulation.force('link').links(links);
 }
 
 update();
@@ -59,7 +56,7 @@ function dragstarted(event, d) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
-    d3.select(this).attr("cursor", "grabbing");
+    d3.select(this).attr('cursor', 'grabbing');
 }
 
 function dragged(event, d) {
@@ -71,5 +68,5 @@ function dragended(event, d) {
     if (!event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
-    d3.select(this).attr("cursor", "grab");
+    d3.select(this).attr('cursor', 'grab');
 }
