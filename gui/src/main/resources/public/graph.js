9da87195a35fdf23175df1f45302c442d8c93083
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2020-12-05 16:44:55 +0100

    added transitions when timeline values are updated..

--- gui/src/main/resources/public/graph.js
+++ gui/src/main/resources/public/graph.js
@@ -171,9 +171,9 @@ ws.onmessage = msg => {
             timelines[i] = tl;
             timelines[i].id = i;
             timelines[i].values.forEach((v, j) => v.id = j);
-            if (timelines[i].type === 'reusable-resource') timelines[i].values.push({ usage: 0, from: timelines[i].values[timelines[i].values.length - 1].to, id: timelines[i].values.length });
+            if (timelines[i].type === 'reusable-resource' && timelines[i].values.length) timelines[i].values.push({ usage: 0, from: timelines[i].values[timelines[i].values.length - 1].to, id: timelines[i].values.length });
         });
-        horizon = d3.max(timelines, d => d.horizon);
+        horizon = Math.max(d3.max(timelines, d => d.horizon), 1);
         timelines_x_scale.domain([0, horizon]);
         timelines_y_scale.domain(d3.range(timelines.length));
         updateTimelines();
@@ -189,7 +189,7 @@ function updateTimelines() {
             return tl_g;
         },
         update => {
-            update.select('rect').attr('width', timelines_x_scale(horizon) + 20);
+            update.select('rect').transition().duration(200).attr('width', timelines_x_scale(horizon) + 20);
             return update;
         }
     );
@@ -209,46 +209,37 @@ function updateTimeline(tl, i) {
                     return tl_val_g;
                 },
                 update => {
+                    update.select('rect').transition().duration(200).attr('x', d => timelines_x_scale(d.from)).attr('y', d => timelines_y_scale(i) + timelines_y_scale.bandwidth() * 0.1).attr('width', d => timelines_x_scale(d.to) - timelines_x_scale(d.from)).attr('height', timelines_y_scale.bandwidth() * 0.9).style('fill', d => sv_value_fill(d));
                     return update;
                 }
             );
             break;
         case 'reusable-resource':
-            const rr_max_val = d3.max(tl.values, d => d.usage);
-            const rr_y_scale = d3.scaleLinear().domain([0, rr_max_val < tl.capacity ? tl.capacity + tl.capacity * 0.1 : rr_max_val + rr_max_val * 0.1]).range([timelines_y_scale(i) + timelines_y_scale.bandwidth(), timelines_y_scale(i)]);
+            const rr_max_val = (tl.values.length ? Math.max(d3.max(tl.values, d => d.usage), tl.capacity) : tl.capacity);
+            const rr_y_scale = d3.scaleLinear().domain([0, rr_max_val + rr_max_val * 0.1]).range([timelines_y_scale(i) + timelines_y_scale.bandwidth(), timelines_y_scale(i)]);
             const rr_g = d3.select('#tl-' + i);
             rr_g.selectAll('path').data([tl.values]).join(
                 enter => {
                     const tl_val_g = enter.append('path').attr('fill', 'aliceblue').attr('stroke', 'lightblue')
-                        .attr('d', d3.area().curve(d3.curveStepAfter)
-                            .x(d => timelines_x_scale(d.from))
-                            .y0(timelines_y_scale(i) + timelines_y_scale.bandwidth())
-                            .y1(d => rr_y_scale(d.usage)));
+                        .attr('d', d3.area().curve(d3.curveStepAfter).x(d => timelines_x_scale(d.from)).y0(timelines_y_scale(i) + timelines_y_scale.bandwidth()).y1(d => rr_y_scale(d.usage)));
                     tl_val_g.on('mouseover', (event, d) => tooltip.html(rr_value_tooltip(d)).transition().duration(200).style('opacity', .9))
                         .on('mousemove', event => tooltip.style('left', (event.pageX) + 'px').style('top', (event.pageY - 28) + 'px'))
                         .on('mouseout', event => tooltip.transition().duration(500).style('opacity', 0));
                     return tl_val_g;
                 },
                 update => {
-                    update.attr('d', d3.area().curve(d3.curveStepAfter)
-                        .x(d => timelines_x_scale(d.from))
-                        .y0(timelines_y_scale(i) + timelines_y_scale.bandwidth())
-                        .y1(d => rr_y_scale(d.usage)));
+                    update.transition().duration(200).attr('d', d3.area().curve(d3.curveStepAfter).x(d => timelines_x_scale(d.from)).y0(timelines_y_scale(i) + timelines_y_scale.bandwidth()).y1(d => rr_y_scale(d.usage)));
                     return update;
                 }
             );
             rr_g.selectAll('line').data([tl.capacity]).join(
                 enter => {
                     const line_g = enter.append('line').attr('stroke-width', 2).attr('stroke-linecap', 'round').attr('stroke', 'darkslategray');
-                    line_g.attr('x1', timelines_x_scale(0)).attr('y1', d =>
-                        rr_y_scale(d)
-                    ).attr('x2', timelines_x_scale(horizon)).attr('y2', d =>
-                        rr_y_scale(d)
-                    );
+                    line_g.attr('x1', timelines_x_scale(0)).attr('y1', d => rr_y_scale(d)).attr('x2', timelines_x_scale(horizon)).attr('y2', d => rr_y_scale(d));
                     return line_g;
                 },
                 update => {
-                    update.attr('y1', d => rr_y_scale(d)).attr('y2', d => rr_y_scale(d));
+                    update.transition().duration(200).attr('y1', d => rr_y_scale(d)).attr('y2', d => rr_y_scale(d));
                     return update;
                 }
             );
