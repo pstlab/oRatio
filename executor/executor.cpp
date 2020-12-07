82b3a160be28f0ab14674afdcc027c2f86c716cd
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2020-12-07 14:15:25 +0100

    introduced a common representation for time..

--- executor/executor.cpp
+++ executor/executor.cpp
@@ -2,6 +2,7 @@
 #include "core.h"
 #include "predicate.h"
 #include "atom.h"
+#include "executor_listener.h"
 #include <thread>
 #include <chrono>
 
@@ -14,48 +15,73 @@ using namespace smt;
 namespace ratio
 {
 
-    executor::executor(core &cr) : cr(cr) {}
+    executor::executor(core &cr, const size_t &tick_dur, const rational &units_for_millis) : cr(cr), tick_duration(tick_dur), units_for_milliseconds(units_for_millis) {}
     executor::~executor() {}
 
-    void executor::set_interval(const size_t &interval)
+    void executor::start()
     {
+        mtx.lock();
         executing = true;
-        std::chrono::high_resolution_clock::time_point start;
-        std::chrono::high_resolution_clock::time_point end;
-        std::thread t([this, &start, &end, &interval]() {
+        mtx.unlock();
+        std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+        std::chrono::high_resolution_clock::time_point end = start;
+        std::thread t([this, &start, &end]() {
             while (true)
             {
                 if (!executing)
                     return;
-                std::this_thread::sleep_for(std::chrono::milliseconds(interval) - (end - start));
+                std::this_thread::sleep_for(std::chrono::milliseconds(tick_duration) - (end - start));
                 if (!executing)
                     return;
                 start = std::chrono::high_resolution_clock::now();
-                // todo: do something..
+                mtx.lock();
+                current_time += tick_duration;
+                for (const auto &l : listeners)
+                    l->tick();
+                if (*pulses.begin() < current_time)
+                {
+                    if (const auto starting_atms = starting_atoms.find(*pulses.begin()); starting_atms != starting_atoms.end())
+                        for (const auto &l : listeners)
+                            l->starting_atoms(starting_atms->second);
+                    if (const auto ending_atms = ending_atoms.find(*pulses.begin()); ending_atms != ending_atoms.end())
+                        for (const auto &l : listeners)
+                            l->ending_atoms(ending_atms->second);
+                }
+                mtx.unlock();
                 end = std::chrono::high_resolution_clock::now();
             }
         });
         t.detach();
     }
 
+    void executor::stop()
+    {
+        mtx.lock();
+        executing = false;
+        mtx.unlock();
+    }
+
     void executor::set_timeout(const size_t &delay)
     {
+        mtx.lock();
         executing = true;
+        mtx.unlock();
         std::thread t([this, delay]() {
             if (!executing)
                 return;
             std::this_thread::sleep_for(std::chrono::milliseconds(delay));
             if (!executing)
                 return;
+            mtx.lock();
             // todo: do something..
+            mtx.unlock();
         });
         t.detach();
     }
 
-    void executor::stop() { executing = false; }
-
     void executor::reset_timelines()
     {
+        mtx.lock();
         starting_atoms.clear();
         ending_atoms.clear();
         pulses.clear();
@@ -87,8 +113,8 @@ namespace ratio
             {
                 arith_expr s_expr = atm->get(START);
                 arith_expr e_expr = atm->get(END);
-                inf_rational start = cr.arith_value(s_expr);
-                inf_rational end = cr.arith_value(e_expr);
+                inf_rational start = cr.arith_value(s_expr) / units_for_milliseconds;
+                inf_rational end = cr.arith_value(e_expr) / units_for_milliseconds;
                 starting_atoms[start].insert(atm);
                 ending_atoms[end].insert(atm);
                 pulses.insert(start);
@@ -97,11 +123,12 @@ namespace ratio
             else if (imp_pred.is_assignable_from(atm->get_type()))
             {
                 arith_expr at_expr = atm->get(AT);
-                inf_rational at = cr.arith_value(at_expr);
+                inf_rational at = cr.arith_value(at_expr) / units_for_milliseconds;
                 starting_atoms[at].insert(atm);
                 ending_atoms[at].insert(atm);
                 pulses.insert(at);
             }
         }
+        mtx.unlock();
     }
 } // namespace ratio
--- executor/executor.h
+++ executor/executor.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "inf_rational.h"
+#include <vector>
+#include <mutex>
 #include <map>
 #include <set>
 
@@ -9,28 +11,38 @@ namespace ratio
 
   class core;
   class atom;
+  class executor_listener;
 
   class executor
   {
+    friend class executor_listener;
+
   public:
-    executor(core &cr);
+    executor(core &cr, const size_t &tick_duration, const smt::rational &units_for_milliseconds = smt::rational(1, 1000));
     executor(const executor &orig) = delete;
     ~executor();
 
     size_t get_current_time() const { return current_time; };
 
-    void set_interval(const size_t &interval);
-    void set_timeout(const size_t &delay);
+    void start();
     void stop();
+    void set_timeout(const size_t &delay);
 
     void reset_timelines();
 
   private:
+    void end_atoms(const std::set<atom *> &atoms);
+
+  private:
     core &cr;
+    const size_t tick_duration;                 // the duration of the ticks in milliseconds..
+    const smt::rational units_for_milliseconds; // the number of plan units for a millisecond (e.g., 1/1000 means one unit for second)..
+    std::vector<executor_listener *> listeners; // the executor listeners..
+    std::mutex mtx;                             // a mutex for the critical sections..
     bool executing = false;
-    size_t current_time = 0;
-    std::map<smt::inf_rational, std::set<atom *>> starting_atoms; // for each pulse, the atoms starting at that pulse..
-    std::map<smt::inf_rational, std::set<atom *>> ending_atoms;   // for each pulse, the atoms ending at that pulse..
-    std::set<smt::inf_rational> pulses;                           // all the pulses of the plan..
+    size_t current_time;
+    std::map<smt::inf_rational, std::set<atom *>> starting_atoms; // for each pulse (in milliseconds), the atoms starting at that pulse..
+    std::map<smt::inf_rational, std::set<atom *>> ending_atoms;   // for each pulse (in milliseconds), the atoms ending at that pulse..
+    std::set<smt::inf_rational> pulses;                           // all the pulses (in milliseconds) of the plan..
   };
 } // namespace ratio
