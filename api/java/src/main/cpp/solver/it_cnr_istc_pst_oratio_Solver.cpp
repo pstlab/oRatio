f87784556ccd47daa7df509aee1b3e22bb6ff3aa
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-02-04 19:09:58 +0100

    solved naming issue..

--- api/java/src/main/cpp/solver/it_cnr_istc_pst_oratio_Solver.cpp
+++ api/java/src/main/cpp/solver/it_cnr_istc_pst_oratio_Solver.cpp
@@ -6,7 +6,7 @@ using namespace ratio;
 
 inline solver *get_solver(JNIEnv *env, jobject obj) { return reinterpret_cast<solver *>(env->GetLongField(obj, env->GetFieldID(env->GetObjectClass(obj), "native_handle", "J"))); }
 
-JNIEXPORT jlong JNICALL Java_it_cnr_istc_oratio_Solver_new_1instance(JNIEnv *env, jobject obj)
+JNIEXPORT jlong JNICALL Java_it_cnr_istc_pst_oratio_Solver_new_1instance(JNIEnv *env, jobject obj)
 {
   solver *s = new solver();
   java_solver_listener *jsl = new java_solver_listener(*s, env, obj);
@@ -14,15 +14,15 @@ JNIEXPORT jlong JNICALL Java_it_cnr_istc_oratio_Solver_new_1instance(JNIEnv *env
   return reinterpret_cast<jlong>(s);
 }
 
-JNIEXPORT void JNICALL Java_it_cnr_istc_oratio_Solver_dispose(JNIEnv *env, jobject obj)
+JNIEXPORT void JNICALL Java_it_cnr_istc_pst_oratio_Solver_dispose(JNIEnv *env, jobject obj)
 {
   delete get_solver(env, obj);
   env->SetLongField(obj, env->GetFieldID(env->GetObjectClass(obj), "native_handle", "J"), 0);
 }
 
-JNIEXPORT void JNICALL Java_it_cnr_istc_oratio_Solver_read__Ljava_lang_String_2(JNIEnv *env, jobject obj, jstring script) { get_solver(env, obj)->read(env->GetStringUTFChars(script, (jboolean) false)); }
+JNIEXPORT void JNICALL Java_it_cnr_istc_pst_oratio_Solver_read__Ljava_lang_String_2(JNIEnv *env, jobject obj, jstring script) { get_solver(env, obj)->read(env->GetStringUTFChars(script, (jboolean) false)); }
 
-JNIEXPORT void JNICALL Java_it_cnr_istc_oratio_Solver_read___3Ljava_lang_String_2(JNIEnv *env, jobject obj, jobjectArray files)
+JNIEXPORT void JNICALL Java_it_cnr_istc_pst_oratio_Solver_read___3Ljava_lang_String_2(JNIEnv *env, jobject obj, jobjectArray files)
 {
   std::vector<std::string> c_files;
   for (jsize i = 0; i < env->GetArrayLength(files); ++i)
@@ -30,4 +30,4 @@ JNIEXPORT void JNICALL Java_it_cnr_istc_oratio_Solver_read___3Ljava_lang_String_
   get_solver(env, obj)->read(c_files);
 }
 
-JNIEXPORT void JNICALL Java_it_cnr_istc_oratio_Solver_solve(JNIEnv *env, jobject obj) { get_solver(env, obj)->solve(); }
+JNIEXPORT void JNICALL Java_it_cnr_istc_pst_oratio_Solver_solve(JNIEnv *env, jobject obj) { get_solver(env, obj)->solve(); }
