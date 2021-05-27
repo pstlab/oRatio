3f785d70850c318e6de849fe0c85675859677aa6
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-05-27 10:35:39 +0200

    minor api improvement..

--- api/java/src/main/java/it/cnr/istc/pst/oratio/timelines/Timeline.java
+++ api/java/src/main/java/it/cnr/istc/pst/oratio/timelines/Timeline.java
@@ -6,6 +6,10 @@ import it.cnr.istc.pst.oratio.InfRational;
 
 public interface Timeline<TV extends TimelineValue> {
 
+    default String getType() {
+        return this.getClass().getSimpleName();
+    }
+
     InfRational getOrigin();
 
     InfRational getHorizon();
