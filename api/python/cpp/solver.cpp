e3b4297ee21cb245fe8417e8c6c76a3bda4a18c2
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-06-09 10:08:59 +0200

    added initial python api..

--- api/python/cpp/solver.cpp
+++ api/python/cpp/solver.cpp
@@ -1,3 +1,8 @@
 #include "solver.h"
 
-static PyObject *test(PyObject *self, PyObject *args) { return nullptr; }
+static PyObject *new_instance(PyObject *self, PyObject *args) { return nullptr; }
+static PyObject *dispose(PyObject *self, PyObject *args) { return nullptr; }
+
+static PyObject *read_riddle(PyObject *self, PyObject *args) { return nullptr; }
+
+static PyObject *solve(PyObject *self, PyObject *args) { return nullptr; }
--- api/python/cpp/solver.h
+++ api/python/cpp/solver.h
@@ -1,3 +1,28 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-static PyObject *test(PyObject *self, PyObject *args);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    static PyObject *new_instance(PyObject *self, PyObject *args);
+    static PyObject *dispose(PyObject *self, PyObject *args);
+
+    static PyObject *read_riddle(PyObject *self, PyObject *args);
+
+    static PyObject *solve(PyObject *self, PyObject *args);
+#ifdef __cplusplus
+}
+#endif
+
+static PyMethodDef methods[] = {
+    {"new_instance", new_instance, METH_VARARGS, "Creates a new oRatio solver instance."},
+    {"dispose", dispose, METH_VARARGS, "Creates a new oRatio solver instance."},
+    {"read", read_riddle, METH_VARARGS, "Creates a new oRatio solver instance."},
+    {"solve", solve, METH_VARARGS, "Creates a new oRatio solver instance."},
+    {NULL, NULL, 0, NULL} /* Sentinel */
+};
+
+static struct PyModuleDef module = {PyModuleDef_HEAD_INIT, "oratio", NULL, -1, methods};
+
+PyMODINIT_FUNC PyInit_oratio(void) { return PyModule_Create(&module); }
