f29856e606b0944dd600caf5af13acb2c3f45d9c
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2021-12-08 09:58:50 +0100

    closed deliberative solver listener..

--- executor/ros/deliberative_solver_listener.cpp
+++ executor/ros/deliberative_solver_listener.cpp
@@ -41,34 +41,77 @@ namespace ratio
         fc_msg.label = f.get_label();
         fc_msg.state = slv.get_sat_core().value(f.get_phi());
         const auto [lb, ub] = slv.get_idl_theory().bounds(f.get_position());
-        fc_msg.lb = lb;
-        fc_msg.ub = ub;
+        fc_msg.lb = lb, fc_msg.ub = ub;
         flaw_created_pub.publish(fc_msg);
     }
     void deliberative_solver_listener::flaw_state_changed(const flaw &f)
     {
+        deliberative_tier::flaw_state_changed fsc_msg;
+        fsc_msg.reasoner_id = d_exec.get_reasoner_id();
+        fsc_msg.flaw_id = reinterpret_cast<std::uintptr_t>(&f);
+        fsc_msg.state = slv.get_sat_core().value(f.get_phi());
+        flaw_state_changed_pub.publish(fsc_msg);
     }
     void deliberative_solver_listener::flaw_cost_changed(const flaw &f)
     {
+        deliberative_tier::flaw_cost_changed fcc_msg;
+        fcc_msg.reasoner_id = d_exec.get_reasoner_id();
+        fcc_msg.flaw_id = reinterpret_cast<std::uintptr_t>(&f);
+        const auto est_cost = f.get_estimated_cost();
+        fcc_msg.cost_num = est_cost.numerator(), fcc_msg.cost_den = est_cost.denominator();
+        flaw_cost_changed_pub.publish(fcc_msg);
     }
     void deliberative_solver_listener::flaw_position_changed(const flaw &f)
     {
+        deliberative_tier::flaw_position_changed fpc_msg;
+        fpc_msg.reasoner_id = d_exec.get_reasoner_id();
+        fpc_msg.flaw_id = reinterpret_cast<std::uintptr_t>(&f);
+        const auto [lb, ub] = slv.get_idl_theory().bounds(f.get_position());
+        fpc_msg.lb = lb, fpc_msg.ub = ub;
+        flaw_position_changed_pub.publish(fpc_msg);
     }
     void deliberative_solver_listener::current_flaw(const flaw &f)
     {
+        deliberative_tier::current_flaw cf_msg;
+        cf_msg.reasoner_id = d_exec.get_reasoner_id();
+        cf_msg.flaw_id = reinterpret_cast<std::uintptr_t>(&f);
+        current_flaw_pub.publish(cf_msg);
     }
 
     void deliberative_solver_listener::resolver_created(const resolver &r)
     {
+        deliberative_tier::resolver_created rc_msg;
+        rc_msg.reasoner_id = d_exec.get_reasoner_id();
+        rc_msg.resolver_id = reinterpret_cast<std::uintptr_t>(&r);
+        rc_msg.effect = reinterpret_cast<std::uintptr_t>(&r.get_effect());
+        rc_msg.label = r.get_label();
+        rc_msg.state = slv.get_sat_core().value(r.get_rho());
+        const auto est_cost = r.get_estimated_cost();
+        rc_msg.cost_num = est_cost.numerator(), rc_msg.cost_den = est_cost.denominator();
+        resolver_created_pub.publish(rc_msg);
     }
     void deliberative_solver_listener::resolver_state_changed(const resolver &r)
     {
+        deliberative_tier::resolver_state_changed rsc_msg;
+        rsc_msg.reasoner_id = d_exec.get_reasoner_id();
+        rsc_msg.resolver_id = reinterpret_cast<std::uintptr_t>(&r);
+        rsc_msg.state = slv.get_sat_core().value(r.get_rho());
+        resolver_state_changed_pub.publish(rsc_msg);
     }
     void deliberative_solver_listener::current_resolver(const resolver &r)
     {
+        deliberative_tier::current_resolver cr_msg;
+        cr_msg.reasoner_id = d_exec.get_reasoner_id();
+        cr_msg.resolver_id = reinterpret_cast<std::uintptr_t>(&r);
+        current_resolver_pub.publish(cr_msg);
     }
 
     void deliberative_solver_listener::causal_link_added(const flaw &f, const resolver &r)
     {
+        deliberative_tier::causal_link_added cl_msg;
+        cl_msg.reasoner_id = d_exec.get_reasoner_id();
+        cl_msg.flaw_id = reinterpret_cast<std::uintptr_t>(&f);
+        cl_msg.resolver_id = reinterpret_cast<std::uintptr_t>(&r);
+        causal_link_added_pub.publish(cl_msg);
     }
 } // namespace ratio
